c6f59137a578400c288df0b3bfdbc3cdb7c86e45
Vinoth Kumar <vinothmaddy061011@gmail.com>
2024-11-06 22:44:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',  // Make sure the base is set to '/' for root deployments
+  build: {
+    outDir: 'dist', // Vite will output the build files to the 'dist' directory
+  },
+});
